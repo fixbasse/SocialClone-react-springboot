1cd38e2fca6c73a8b0838a4d28ef8b4219996f1d
fixbasse <fix_kaow@hotmail.com>
2024-02-13 00:28:18 +0700

    update

--- client/src/pages/Unprotected/Right.tsx
+++ client/src/pages/Unprotected/Right.tsx
@@ -8,7 +8,6 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { SignUpModal } from "@/components/modals/SignUpModal"
